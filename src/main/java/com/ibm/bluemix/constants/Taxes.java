6ac141c05fc143600982b551a82e3393360a40ea
omar meshaal <omar.mesh@eg.ibm.com>
2017-06-06 18:01:18 +0200

    reorg order of methods

---
+++ src/main/java/com/ibm/bluemix/constants/Taxes.java
@@ -0,0 +1,9 @@
+package com.ibm.bluemix.constants;
+
+/**
+ Created by omar on 06-06-2017.
+ */
+public class Taxes {
+  public static final double BASIC_TAX = 0.10;
+  public static final double IMPORT_TAX = 0.05;
+}
